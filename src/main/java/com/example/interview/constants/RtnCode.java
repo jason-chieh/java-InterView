9267031fd533d169ac1b59a2a5ee3a89866ea084
jason-g <a2012886@yahoo.com.tw>
2024-03-12 22:23:33 +0800

    test

--- src/main/java/com/example/interview/constants/RtnCode.java
+++ src/main/java/com/example/interview/constants/RtnCode.java
@@ -3,7 +3,7 @@ package com.example.interview.constants;
 public enum RtnCode {
 
 	SUCCESSFUL(200,"OK"), //
-	ERROR_VOTENAME_NONE(400,"ERROR_VOTENAME_NONE"), //
+	ERROR_VOTENAME_NONE(400,"ERROR_VOTENAME_NONE"), // ด๚ธี
 	FIND_VOTENAME_NONE(400,"FIND_VOTENAME_NONE"), //
 	VOTENAME_NONE(400,"VOTENAME_NONE"), //
 	;
